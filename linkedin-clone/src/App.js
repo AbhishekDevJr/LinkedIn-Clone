11ccc49affeeeed2171a6f4f4b84a88821640077
Abhishek Choudhari <abhishek007coc@gmail.com>
2023-03-01 20:11:56 +0530

    Building Login Component and Login Flow Logic

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -4,11 +4,17 @@ import SideBar from './components/SideBar';
 import Feed from './components/Feed';
 import NewsSection from './components/NewsSection';
 import SearchIcon from '@mui/icons-material/Search';
+import { useSelector } from 'react-redux';
+import { selectUser } from './components/features/userSlice';
+import Login from './Login';
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="App">
       <Header />
+      {(!user) ? (<Login />) : (
       <div className = "main-section">
         {/* SideBar */}
         <SideBar className = "main-child1" />
@@ -17,6 +23,8 @@ function App() {
         {/* LinkedInNews */}
         <NewsSection className = "main-child3" />
       </div>
+      )
+      }
     </div>
   );
 }
---
+++ linkedin-clone/src/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Login(){
+    return(
+        <div className = "login">
+            This is Login Component
+        </div>
+    );
+}
+
+export default Login;
